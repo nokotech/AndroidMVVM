1a5c47b28a1719e7725d3db82f91bcae98c51855
TakenokoTech <takenoko.tech@gmail.com>
2018-02-14 15:03:57 +0900

    fix comment

--- app/src/main/java/tech/takenoko/androidmvvm/common/BaseUsecase.kt
+++ app/src/main/java/tech/takenoko/androidmvvm/common/BaseUsecase.kt
@@ -1,5 +1,7 @@
 package tech.takenoko.androidmvvm.common
 
+import android.databinding.ObservableField
+
 /**
  * Created by takenoko on 2018/02/11.
  */
@@ -8,9 +10,17 @@ abstract class BaseUsecase() {
     abstract val log: String;
 
     /**
+     * (Extensions) change value and notify.
+     */
+    fun <T> ObservableField<T>.update(value: T, notify: Int) {
+        this.set(value)
+        notifyPropertyChanged(notify);
+    }
+
+    /**
      * Mock Function1
      */
-    fun function1() {
+    fun function1(viewModel: BaseViewModel) {
         // BaseRepository.func1()
     }
 }
--- app/src/main/java/tech/takenoko/androidmvvm/page_sample2/Sample2_Usecase.kt
+++ app/src/main/java/tech/takenoko/androidmvvm/page_sample2/Sample2_Usecase.kt
@@ -1,6 +1,6 @@
 package tech.takenoko.androidmvvm.page_sample2
 
-import android.databinding.ObservableField
+import tech.takenoko.androidmvvm.BR
 import tech.takenoko.androidmvvm.Const
 import tech.takenoko.androidmvvm.common.BaseUsecase
 import tech.takenoko.androidmvvm.utility.ULog
@@ -18,11 +18,13 @@ class Sample2_Usecase @Inject constructor(): BaseUsecase() {
     @Inject lateinit var repository: Sample2_Repository
 
     /**
-     *
+     * get repository data.
+     * - ViewModel.bindingProperty(ObservableField) --> (Observable)
+     * - notifyPropertyChanged(BR.xxx);
      */
-    fun getSampleText(sampleText: ObservableField<String>) {
+    fun getSampleText(viewModel: Sample2_ViewModel) {
         ULog.debug("Sample2_Usecase", "getSampleText.")
-        sampleText.set("loding....")
-        repository.getSampleText(sampleText, Const.ReadType.API)
+        viewModel.sampleText.update("loding....", BR._all)
+        repository.getSampleText(viewModel.sampleText, Const.ReadType.API)
     }
 }
