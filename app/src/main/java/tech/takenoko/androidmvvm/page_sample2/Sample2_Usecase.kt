7dceebc9b6b45ca49061442a071a4fc02fc0e8b2
takenoko <takenoko.tech@gmail.com>
2018-02-14 17:17:27 +0900

    enh api.

--- app/src/main/java/tech/takenoko/androidmvvm/page_sample2/Sample2_Usecase.kt
+++ app/src/main/java/tech/takenoko/androidmvvm/page_sample2/Sample2_Usecase.kt
@@ -1,9 +1,17 @@
 package tech.takenoko.androidmvvm.page_sample2
 
+import io.reactivex.observers.DisposableSingleObserver
+import rx.Single
+import rx.SingleSubscriber
+import rx.internal.operators.SingleObserveOn
 import tech.takenoko.androidmvvm.BR
 import tech.takenoko.androidmvvm.Const
+import tech.takenoko.androidmvvm.api.Sample_Api
 import tech.takenoko.androidmvvm.common.BaseUsecase
+import tech.takenoko.androidmvvm.common.CustomSubscriber
 import tech.takenoko.androidmvvm.utility.ULog
+import tech.takenoko.androidmvvm.utility.Util
+import java.util.*
 import javax.inject.Inject
 
 
@@ -24,7 +32,21 @@ class Sample2_Usecase @Inject constructor(): BaseUsecase() {
      */
     fun getSampleText(viewModel: Sample2_ViewModel) {
         ULog.debug("Sample2_Usecase", "getSampleText.")
+
+        // loading.
         viewModel.sampleText.update("loding....", BR._all)
-        repository.getSampleText(viewModel.sampleText, Const.ReadType.API)
+
+        // define subscriber.
+        val subscriber = object: SingleSubscriber<Sample_Api.GetLatestEntity>() {
+            override fun onSuccess(t: Sample_Api.GetLatestEntity?) {
+                viewModel.sampleText.update(t?.base?: "", BR._all)
+            }
+            override fun onError(error: Throwable?) {
+                viewModel.sampleText.update("Error", BR._all)
+            }
+        }
+
+        // get repository.
+        Single.create<Sample_Api.GetLatestEntity> { repository.getLatest(it, Const.ReadType.API) }.subscribe(subscriber)
     }
 }
