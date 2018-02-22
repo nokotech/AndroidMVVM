2edfb4598fbff5ce9e266f05a383e97734cc0d6b
takenoko <takenoko.tech@gmail.com>
2018-02-23 01:41:15 +0900

    fix usecase.

--- app/src/main/java/tech/takenoko/androidmvvm/page_sample2/Sample2_Usecase.kt
+++ app/src/main/java/tech/takenoko/androidmvvm/page_sample2/Sample2_Usecase.kt
@@ -1,15 +1,18 @@
 package tech.takenoko.androidmvvm.page_sample2
 
 import io.reactivex.android.schedulers.AndroidSchedulers
-import rx.SingleSubscriber
-import rx.schedulers.Schedulers
+import rx.Single
+import rx.Subscriber
 import tech.takenoko.androidmvvm.BR
 import tech.takenoko.androidmvvm.Const
 import tech.takenoko.androidmvvm.api.Sample_Api
-import tech.takenoko.androidmvvm.common.BaseCustomAdapter
 import tech.takenoko.androidmvvm.common.BaseUsecase
 import tech.takenoko.androidmvvm.utility.ULog
 import javax.inject.Inject
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import rx.Observable
 
 
 /**
@@ -34,9 +37,11 @@ class Sample2_Usecase @Inject constructor(): BaseUsecase() {
         viewModel.sampleText.update("loding....", BR.sampleText)
 
         // define subscriber.
-        val subscriber = object: SingleSubscriber<Sample_Api.GetLatestEntity>() {
-            override fun onSuccess(t: Sample_Api.GetLatestEntity?) {
-
+        val subscriber = object: Subscriber<Sample_Api.GetLatestEntity>() {
+            override fun onNext(t: Sample_Api.GetLatestEntity?) {
+                ULog.debug("Sample2_Usecase", "subscriber.onNext")
+            }
+            override fun onCompleted() {
                 val latest: Sample_Api.GetLatestEntity? = repository.getPropaty()["latest"]
                 val past: Sample_Api.GetLatestEntity? = repository.getPropaty()["past"]
 
@@ -45,8 +50,6 @@ class Sample2_Usecase @Inject constructor(): BaseUsecase() {
                     val text1 = "${it.key} / ${latest.base}"
                     val text2 = "${it.value}"
                     // calc
-//                    val past = past?.rates?.get(it.key)
-//                    ULog.debug("=====", past?:"")
 //                    val sub = BigDecimal(it.value) - BigDecimal(past?.rates?.get(it.key))
 //                    val rate = (sub / BigDecimal(text2)).multiply(BigDecimal(100)) + BigDecimal(1)
                     // insert
@@ -56,40 +59,30 @@ class Sample2_Usecase @Inject constructor(): BaseUsecase() {
                         row.text1 = text1
                         row.text2 = text2
                         row.text3 = 0//rate.toInt()
-                        viewModel.latestButtonList[index] = row
+//                        viewModel.latestButtonList[index] = row
                     } else {
-                        viewModel.latestButtonList.add(BaseCustomAdapter.SampleList(text1, text2, 0/*rate.toInt()*/))
+//                        viewModel.latestButtonList.add(BaseCustomAdapter.SampleList(text1, text2, 0/*rate.toInt()*/))
                     }
                 }
                 // sort.
-                // viewModel.latestButtonList.sortByDescending { java.lang.Float.parseFloat(it.text2) }
-
+                viewModel.latestButtonList.sortByDescending { java.lang.Float.parseFloat(it.text2) }
                 // API call sucess.
+                ULog.info("subscriber", "onCompleted called.")
                 viewModel.sampleText.update("Success [${latest?.date}]", BR._all)
             }
             override fun onError(error: Throwable?) {
                 // API call error.
-                ULog.error("onError", error.toString())
-                viewModel.sampleText.update("Error", BR._all)
-            }
-        }
-
-        val nextSubscriber = object: SingleSubscriber<Sample_Api.GetLatestEntity>() {
-            override fun onSuccess(t: Sample_Api.GetLatestEntity?) {
-                repository.getPast("2017-02-18", "USD", "JPY", Const.ReadType.PROPERTY)
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(subscriber)
-            }
-            override fun onError(error: Throwable?) {
-                // API call error.
-                ULog.error("onError", error.toString())
+                ULog.error("subscriber", "onError called. ${error.toString()}")
                 viewModel.sampleText.update("Error", BR._all)
             }
         }
         // get repository.
-        repository.getLatest("USD", "JPY", Const.ReadType.PROPERTY).subscribe(nextSubscriber)
+        Observable.merge(
+                repository.getLatest("USD", "JPY", Const.ReadType.PROPERTY).toObservable(),
+                repository.getPast("2017-02-18", "USD", "JPY", Const.ReadType.PROPERTY).toObservable()
+        ).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(subscriber)
 
+        Sub
     }
 
 }
