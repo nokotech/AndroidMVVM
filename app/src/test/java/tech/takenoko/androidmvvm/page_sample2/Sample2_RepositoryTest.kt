bea0fec11731b29c49ecb1365952b5825a644f0c
takenoko <takenoko.tech@gmail.com>
2018-03-11 07:31:29 +0900

    add Mockito

---
+++ app/src/test/java/tech/takenoko/androidmvvm/page_sample2/Sample2_RepositoryTest.kt
@@ -0,0 +1,83 @@
+package tech.takenoko.androidmvvm.page_sample2
+
+import android.util.Log
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+import rx.Single
+import rx.Single.create
+import rx.SingleSubscriber
+import rx.observers.TestSubscriber
+import tech.takenoko.androidmvvm.RxSingleSubscriber
+import tech.takenoko.androidmvvm.api.Sample_Api
+import tech.takenoko.androidmvvm.cache.Sample_Cache
+import tech.takenoko.androidmvvm.database.Sample_Dao
+import tech.takenoko.androidmvvm.database.Sample_Table
+import tech.takenoko.androidmvvm.utility.ApiBuilder
+
+/**
+ * Created by takenoko on 2018/03/11.
+ */
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(Sample2_Repository::class, Sample_Dao::class, Sample_Api::class, ApiBuilder::class, Log::class)
+class Sample2_RepositoryTest {
+
+    val list: MutableList<Sample_Table> = mutableListOf()
+    val sampleDao:Sample_Dao = PowerMockito.mock(Sample_Dao::class.java)
+    val sampleApi = mock(Sample_Api::class.java)
+    val sample2Repository = Sample2_Repository()
+
+    @Before
+    fun setUp() {
+        PowerMockito.mockStatic(Log::class.java)
+        PowerMockito.mockStatic(Sample_Api::class.java)
+        sample2Repository.sampleCache = Sample_Cache()
+        sample2Repository.sampleDao = sampleDao
+        sample2Repository.sampleApi = sampleApi
+    }
+
+    @Test
+    fun getLatest() {
+        `when`(sampleDao.findByBaseAndDate("", "")).thenReturn(list)
+        `when`(sampleApi.getLatest(ArgumentMatchers.anyString())).thenReturn(null)
+        val single = sample2Repository.getLatest("", "")
+        assertEquals(null, single)
+    }
+
+    fun createSingle(): Single<Sample_Api.GetLatestEntity>? {
+        return create(object : Single.OnSubscribe<Sample_Api.GetLatestEntity> {
+            override fun call(t: SingleSubscriber<in Sample_Api.GetLatestEntity>?) {
+                val map: MutableMap<String, String> = mutableMapOf()
+                map.put("aaa", "bbb")
+                val entity = Sample_Api.GetLatestEntity("a", "b", map)
+                if (t != null) {
+                    t.onSuccess(entity)
+                }
+            }
+        })
+    }
+
+    @Test
+    fun getPast() {
+        `when`(sampleDao.findByBaseAndDate("", "")).thenReturn(list)
+        `when`(sampleApi.getPast(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(createSingle())
+        val single = sample2Repository.getPast("99991230", "aaa", "bbb")
+        val testSubscriber = TestSubscriber<Any>()
+
+        val apiSubscriber = RxSingleSubscriber<List<Sample_Cache.Entity>>("Sample2_Repository.getPast")
+        apiSubscriber.setSuccessBlock{ t ->
+            assertEquals(null, t)
+        }
+        single.subscribe(testSubscriber)
+        testSubscriber.assertNoErrors()
+        testSubscriber.assertValues(single)
+    }
+
+}
